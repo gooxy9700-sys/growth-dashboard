8029b51e982c34338fdd28195bffeb5bf57f05f6
XY9700 <dyx060616@qq.com>
2026-08-24 20:20:15 +0800

    Auto start breaks and show focus share ring

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yijishu-v21";
+const CACHE_NAME = "yijishu-v22";
 const ASSETS = ["./", "./index.html", "./styles.css", "./supabase-config.js", "./schedule-data.js", "./plan-data.js", "./app.js", "./pomodoro-enhancements.js", "./health-kit-adapter.js", "./sync.js", "./privacy.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
